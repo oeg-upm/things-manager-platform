fecc4057451963525963f17702c0fb39f043f693
Andrea Cimmino <andreacimmino@MacBook-Pro-de-Andrea.local>
2022-01-10 13:23:02 +0100

    dynamic config

--- src/main/java/oeg/cogito/thingmanager/TMConfiguration.java
+++ src/main/java/oeg/cogito/thingmanager/TMConfiguration.java
@@ -2,8 +2,8 @@ package oeg.cogito.thingmanager;
 
 public class TMConfiguration {
 
-	public static String triplestore  = "http://localhost:8890/sparql";
-	public static String WoTHive = "https://wothive.linkeddata.es";//
+	public static String triplestore  = null;
+	public static String WoTHive = null;//
 	
 	
 	
--- src/main/java/oeg/cogito/thingmanager/ThingManagerApplication.java
+++ src/main/java/oeg/cogito/thingmanager/ThingManagerApplication.java
@@ -1,8 +1,17 @@
 package oeg.cogito.thingmanager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.gson.JsonObject;
 
 import ch.rasc.sse.eventbus.config.EnableSseEventBus;
 
@@ -11,8 +20,53 @@ import ch.rasc.sse.eventbus.config.EnableSseEventBus;
 @EnableSseEventBus
 public class ThingManagerApplication {
 	
+	private static final String CONFIG_FILE = "./tm-config.json";
+	private static final String CONFIG_TDD_KEY = "wot directory";
+	private static final String CONFIG_TSTORE_KEY = "triple store";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ThingManagerApplication.class, args);
+		File config = new File(CONFIG_FILE);
+		if(!config.exists()) {
+			System.out.println("No configuration file found! providing a default one ");
+			try {
+				FileUtils.writeByteArrayToFile(config, errorMessage().replace("'", "").getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+		
+		try{
+			 String text = new String(Files.readAllBytes(Paths.get(CONFIG_FILE)), StandardCharsets.UTF_8);
+			 JsonObject json = TMUtils.toJson(text);
+			 if(!json.has(CONFIG_TDD_KEY)) {
+				System.out.println("Missing '"+CONFIG_TDD_KEY+"' mandatory key in 'tm-config.json', please provide a json similar to: ");
+				System.out.println(errorMessage());
+				System.exit(-1); 
+			 }
+			 if(!json.has(CONFIG_TSTORE_KEY)) {
+				System.out.println("Missing '"+CONFIG_TSTORE_KEY+"' mandatory key in 'tm-config.json', please provide a json similar to: ");
+				System.out.println(errorMessage());
+				System.exit(-1);
+			 }
+			 TMConfiguration.triplestore = json.get(CONFIG_TSTORE_KEY).getAsString();
+			 TMConfiguration.WoTHive = json.get(CONFIG_TDD_KEY).getAsString();
+			 System.out.println("----| Configuration: wot directory is "+TMConfiguration.WoTHive);
+			 System.out.println("----| Configuration: triple store is "+TMConfiguration.triplestore);
+
+		}catch(Exception e) {
+			System.out.println(e.toString());
+			System.exit(-1);
+		}
+
+	}
+	
+	private static String errorMessage() {
+		return "'{ "
+				+ "\n  \"wot directory\" : \"https://wothive.linkeddata.es\", "
+				+ "\n  \"triple store\" : \"http://localhost:8890/sparql\""
+				+ "\n  }' \n";
 	}
 
 }
